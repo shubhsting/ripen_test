705f4a8cfbf8a34345cf6fb2d89113bcd4104cd9
shubham <shubham@shubhams-MacBook-Air.local>
2021-11-26 00:02:38 +0530

    updated

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -3,6 +3,24 @@ const { catchAsyncErrors } = require("../middleware/catchAsyncErrors");
 const sendWelcomeMail = require("./testemail");
 const register = catchAsyncErrors(async (req, res) => {
   const check = await User.findOne({ email: req.body.email }).exec();
+  const phone =req.body.phone_number;
+  if(phone){
+    const check2 = await User.findOne({ phone_number: phone }).exec();
+    if(check2 && check2.is_verified){
+      return res.status(409).send({message:"user already exists"});
+    }
+    else if(check2){
+      await User.findOneAndDelete({
+        email: req.body.email
+      });
+      await User.updateOne(
+        { phone_number: phone },
+        { $set: { email: req.body.email } }
+      );
+      return res.status(200).send({ msg: "succesfully joined" });
+    }
+  }
+  
   if(check && check.is_verified){
     return res.status(409).send({message:"user already exists"});
   }
@@ -18,7 +36,6 @@ const register = catchAsyncErrors(async (req, res) => {
     const num = req.body.phone_number;
     const newDoc = { email: req.body.email, phone_number: num ? num : "" };
     const result = await User.create(newDoc);
-    console.log("user added " + result);
     res.status(200).json({ msg: "User successfully added" });
     sendWelcomeMail(req.body.email);
   }
